2b6c98484f4366fed735bf8c4f0581bd655565e5
pablodg12 <pablo.ibarras@alumnos.usm.cl>
2020-12-08 22:31:37 -0300

    update

--- step-swager/deploy.sh
+++ step-swager/deploy.sh
@@ -17,12 +17,12 @@ fi
 
 FILENAME=$(echo $RANDOM.${STACK_NAME} | openssl dgst -sha1 | sed 's/^.* //')
 BUCKET="s3://$S3_BUCKET/$STACK_NAME/$FILENAME"
-BUCKET_FOLDER="s3://$S3_BUCKET/$STACK_NAME/"
+BUCKET_FOLDER="s3://$S3_BUCKET/$STACK_NAME/openapi.yaml"
 
 echo ${BUCKET}
 echo ${FILENAME}
 
-aws s3 cp openapi.yaml ${BUCKET_FOLDER} --sse
+aws s3 cp step-swager/openapi.yaml ${BUCKET_FOLDER} --sse
 
-sam build --template-file template.yaml && sam package --output-template-file packaged.yaml --s3-bucket ${S3_BUCKET} \
-&& sam deploy --template-file packaged.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --parameter-overrides OpenAPIS3File=${S3_OPEN_API}
+sam build --template-file step-swager/template.yaml && sam package --output-template-file packaged.yaml --s3-bucket ${S3_BUCKET} \
+&& sam deploy --template-file packaged.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --parameter-overrides OpenAPIS3File=${BUCKET_FOLDER}
